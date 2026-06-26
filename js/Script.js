e24a69d0886e0e532f6d577c8589d0909c96067d
Hélio Marcos <h.paixao@aluno.senai.br>
2026-06-26 18:40:41 -0300

    Inicio do Script

---
+++ js/Script.js
@@ -0,0 +1,15 @@
+const formDados = document.querySelector('#formulario');
+const divResultado = document.querySelector('#resultado');
+
+formDados.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const formData = new FormData(formDados);
+
+     // Pegando os valores 
+     let des = formData.get('des'); 
+     let com = parseFloat(formData.get('com'));
+     let lar = parseFloat(formData.get('lar'));
+
+  // Cálculo 
+  let cal = peso / (alt * alt);
