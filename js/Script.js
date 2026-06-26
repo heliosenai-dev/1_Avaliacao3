c52ef29df47054cde6269ed8418f4efbeb6ec5e5
Hélio Marcos <h.paixao@aluno.senai.br>
2026-06-26 19:12:53 -0300

    Fazendo Alterações pois o codigo não quer imprimir o resultado

--- js/Script.js
+++ js/Script.js
@@ -14,4 +14,25 @@ formDados.addEventListener('submit', (evt) => {
   // Cálculo 
   let are = com * lar
   let valorm = are * 550
+
+  if (valorm <= 20.000) {
+    divResultado.innerHTML = `Isento ${valorm.toFixed(2)})`;
+} else if (valorm >= 20.000 && valorm < 100.000) {
+    divResultado.innerHTML = `Valor Adicional é de 5%${toFixed(2)})`;
+} else if (valorm >= 100.000 && valorm < 500.000) {
+    divResultado.innerHTML = `Valor Adicional é de 10% ${toFixed(2)})`;
+} else if (valorm >= 500.00 && valorm <= 1000.000) {
+    divResultado.innerHTML = `Valor Adicional é de 15% ${toFixed(2)})`;
+} else {valorm > 1000.000
+    divResultado.innerHTML = `Valor Adicional é de 20%  ${toFixed(2)})`;
+}
+
+item.innerHTML = `
+            <p><b>Descrição do lote:</b> ${des}</p>
+            <p><b>Comprimento:</b> ${com}</p>
+            <p><b>Largura:</b> ${lar}</p>
+            <p><b>Área (m2):</b> ${are}</p>
+            <p><b>Valor do lote :</b> R$ ${valorm.toFixed(2)}</p>
+       `
+       
 })
